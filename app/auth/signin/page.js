402eb27c152e77ecd81d43c00a7e0f3679b33219
adenisrael97@gmail.com <adenisrael97@gmail.com>
2025-07-04 12:57:18 +0100

    first commit

---
+++ app/auth/signin/page.js
@@ -0,0 +1,12 @@
+export default function () {
+  return (
+    <main>
+      <div className="flex justify-center items-center mt-50">
+        <button className="w-[200px] h-[30px] bg-blue-300 rounded-2xl">
+          {" "}
+          signin with Google
+        </button>
+      </div>
+    </main>
+  );
+}
---
+++ app/dashboard/profile/page.js
@@ -0,0 +1,10 @@
+import { FaTwitter } from "react-icons/fa6";
+
+export default function profile() {
+  return (
+    <main>
+      profile page
+      <FaTwitter />
+    </main>
+  );
+}
